9c7f76f814ebf0be913b300509dcae822b6f4bb7
Garrett Hays <GarrettlHays@gmail.com>
2022-09-07 15:24:36 -0700

    updates bird.js to include popovers, dropdowns and other bootstrap elements
    Co-authored-by: Sandra Tena <SandraT22@users.noreply.github.com>
    Co-authored-by: Anthony DiFalco <Di-Falco@users.noreply.github.com>

--- src/components/Bird.js
+++ src/components/Bird.js
@@ -1,17 +1,44 @@
 import React from "react";
 import PropTypes from "prop-types";
+import Button from 'react-bootstrap/Button';
+import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
+import Popover from 'react-bootstrap/Popover';
+import ButtonGroup from 'react-bootstrap/ButtonGroup';
+import Dropdown from 'react-bootstrap/Dropdown';
 
 function Bird(props){
+  
+  const popover = (
+    <Popover id="popover-basic">
+      <Popover.Header as="h3">{props.commName} Details</Popover.Header>
+      <Popover.Body>
+        {props.sciName !== "" ? <li>Latin Name: <em>{props.sciName}</em></li> : null}
+        <li>Location: {props.location}</li>
+        <li>Date Seen: {props.date}</li>
+        {props.appearance !== "" ? <li>Appearance: {props.appearance}</li> : null}
+        {props.activity !== "" ? <li>Activity: {props.activity}</li> : null}
+      </Popover.Body>
+    </Popover>
+  );
+  
+  const Example = () => (
+      <Dropdown as={ButtonGroup}>
+        <OverlayTrigger trigger="click" placement="right" overlay={popover}>
+        <Button variant="info">{props.commName}</Button>
+        </OverlayTrigger>
+        <Dropdown.Toggle split variant="dark" id="dropdown-split-basic" />
+        <Dropdown.Menu>
+        <Dropdown.Item href="#/edit">Edit Bird</Dropdown.Item>
+        <Dropdown.Item href="#/delete">Destroy Bird</Dropdown.Item>
+        </Dropdown.Menu>
+      </Dropdown>
+  );
+  
   return (
-    <React.Fragment>
-      <h3><em>{props.commName}</em></h3>
-      <h4>{props.sciName}</h4>
-      <p>{props.location}</p>
-      <p>{props.date}</p>
-      <p>{props.appearance}</p>
-      <p>{props.activity}</p>
-      <hr/>
-    </React.Fragment>
+    <div className='mb-3'>
+      <Example />
+      <br />
+    </div>
   );
 }
 
@@ -25,3 +52,4 @@ Bird.propTypes = {
 };
 
 export default Bird;
+
